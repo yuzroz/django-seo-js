d9ae0fac646488d0e1a201ae2ac310a4193a976a
yuzroz <yuzuruozawa@gmail.com>
2015-02-25 14:11:48 +0900

    Add realtime request timeout setting

--- django_seo_js/backends/prerender.py
+++ django_seo_js/backends/prerender.py
@@ -29,7 +29,7 @@ class PrerenderIO(SEOBackendBase, RequestsBasedBackend):
             'X-Prerender-Token': self.token,
             'Accept-Encoding': 'gzip',
         }
-        r = self.requests.get(render_url, headers=headers)
+        r = self.requests.get(render_url, headers=headers, timeout=settings.SEO_JS_REALTIME_TIMEOUT)
         assert int(r.status_code) < 500
 
         return self.build_django_response_from_requests_response(r)
